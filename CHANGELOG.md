62e983879be5f4714b38459cf49e66c4c643af2b
Paolo Lazzari <lazzari.paolok@gmail.com>
2023-05-19 13:30:24 +0200

    Bump initial version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
-### v0.1.0
+### v0.1.1
 
 - Initial commit, add support for python runtimes
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="lambda2docker",
-    version="0.1.0",
+    version="0.1.1",
     description=("Generate a Dockerfile from a lambda function"),
     keywords="aws, lambda, docker",
     author="paololazzari",
